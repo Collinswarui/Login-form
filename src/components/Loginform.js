4339a6ac91d053b8302edd98e97e693068631e51
collins <collinswanjiru47@gmail.com>
2023-07-22 14:10:55 +0300

    Initial commit

---
+++ src/components/Loginform.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:5000/login', {
+        username,
+        password,
+      });
+
+      setMessage(response.data.message);
+      setTimeout( () => {
+        setUsername('');
+        setPassword('');
+      }, 1000);
+      
+    } catch (error) {
+        if (error.response && error.response.data) {
+            setMessage(error.response.data.error);
+        } else {
+            setMessage('An error occcurred. Please try again later');;
+        }
+      
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            autoComplete='off'
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            autoComplete='off'
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ src/components/Signupform.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Signupform = () => {
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost:5000/signup', {
+                firstname,
+                lastname,
+                username,
+                email,
+                password,
+            });
+            if (response && response.data) {
+             setMessage(response.data.message);
+             setFirstname('');
+             setLastname('');
+             setUsername('');
+             setEmail('');
+             setPassword('');
+            }
+
+        } catch (error) {
+            if (error.response && error.response.data) {
+                setMessage(error.response.data.error);
+            } else {
+                setMessage('An error occurred. Please try again later.');
+            }
+        }
+    };
+
+
+    return (
+        <div>
+            <h2>Sign Up</h2>
+            <form onSubmit={handleSubmit}>
+            <label>
+          Firstname:
+          <input
+            type="text"
+            value={firstname}
+            onChange={(e) => setFirstname(e.target.value)}
+          />
+        </label>
+         <br />
+            <label>
+          Lastname:
+          <input
+            type="text"
+            value={lastname}
+            onChange={(e) => setLastname(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Sign Up</button>
+      </form>
+      <p>{message}</p>
+        </div>
+    );
+};
+
+export default Signupform;
